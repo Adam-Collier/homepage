403629d404b1c1c4cf8444bd8368cfffab4cdd66
admjacqus <adam.jacques@missguided.com>
2018-08-28 14:56:16 +0100

    starting WC 27

--- gulpfile.js
+++ gulpfile.js
@@ -22,7 +22,7 @@ gulp.task("default", function() {
   browserSync.init({
     port: 7184,
     // https: true,
-    ghostMode: true,
+    ghostMode: false,
     server: {
       baseDir: "./"
     }
